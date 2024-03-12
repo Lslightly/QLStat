9c2737d07be217f548d79957e9dc6e6bc6e1d158
lqw@128C <lqw332664203@mail.ustc.edu.cn>
2024-03-12 09:35:34 +0800

    添加说明检测有效的例子

--- test/repos/intraResourceLeak/main.go
+++ test/repos/intraResourceLeak/main.go
@@ -7,6 +7,39 @@ import (
 )
 
 func main() {
+	aliasNotClosed()
+	notClosed()
+}
+
+func closed() {
+	f, err := os.Open("go.mod")
+	defer f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var line string
+	ptrf := &f
+	falias := *ptrf
+	ptrf2 := &falias
+	fmt.Fscanf(*ptrf2, "%s", &line)
+	fmt.Println(line)
+}
+
+func aliasNotClosed() {
+	f, err := os.Open("go.mod")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var line string
+	ptrf := &f
+	falias := *ptrf
+	ptrf2 := &falias
+	fmt.Fscanf(*ptrf2, "%s", &line)
+	fmt.Println(line)
+	(*ptrf2).Close()
+}
+
+func notClosed() {
 	f, err := os.Open("go.mod")
 	if err != nil {
 		log.Fatal(err)
