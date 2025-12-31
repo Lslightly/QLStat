1543d60162dd07c896cf40c6c5018b4daeb991eb
lqw@128C <lqw332664203@mail.ustc.edu.cn>
2025-12-31 16:33:57 +0800

    escape_adapter: deduplicate rows

--- cmd/escape_adapter/movedtoheap.go
+++ cmd/escape_adapter/movedtoheap.go
@@ -29,9 +29,19 @@ func cleanpath(path string) string {
 	return filepath.Clean(filepath.Join(absSrcRoot, path))
 }
 
+type MovedToHeapRow struct {
+	path                string
+	startLine, startCol int
+}
+
+func (row *MovedToHeapRow) String() string {
+	const pat string = "%s,%d,%d"
+	return fmt.Sprintf(pat, row.path, row.startLine, row.startCol)
+}
+
 // path, startLine, startCol
 func movedToHeapHandle(lineGen LineGenerator) (csvRows []string) {
-	const pat string = "%s,%d,%d"
+	rowSet := make(map[MovedToHeapRow]bool)
 	for i, line := range lineGen {
 		if !strings.Contains(line, "moved to heap") {
 			continue
@@ -43,7 +53,19 @@ func movedToHeapHandle(lineGen LineGenerator) (csvRows []string) {
 			continue
 		}
 		path, startLineStr, startColStr := matches[1], matches[2], matches[3]
-		csvRows = append(csvRows, fmt.Sprintf(pat, cleanpath(path), convint(startLineStr), convint(startColStr)))
+		row := MovedToHeapRow{
+			path:      cleanpath(path),
+			startLine: convint(startLineStr),
+			startCol:  convint(startColStr),
+		}
+		if rowSet[row] {
+			continue
+		}
+		rowSet[row] = true
+	}
+	csvRows = make([]string, 0, len(rowSet))
+	for row := range rowSet {
+		csvRows = append(csvRows, row.String())
 	}
 	return
 }
