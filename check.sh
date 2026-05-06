9f6d0cca5be11c4cd1727f07fa672cc2ca5508ea
lqw@128C <lqw332664203@mail.ustc.edu.cn>
2026-05-06 10:22:34 +0800

    maintain(check): add external verify test

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 function splitline {
     echo "-----------------"
@@ -11,10 +11,18 @@ if ! go test -v ./cmd/batch_clone_build; then
 fi
 
 splitline
-
 echo "Escape Adapter Test"
 if ! go test -v ./cmd/escape_adapter; then
     echo "Escape Adapter Test Failed"
+    exit 1
+fi
+
+splitline
+echo "External Verify Test"
+
+go run ./cmd/batch_clone_build -noclone yaml-examples/malloc_test.yaml
+if ! go run ./cmd/pprof-external-verify codeql-db/test/malloc_test/ instance_count ; then
+    echo "External Verify Test for malloc_test Failed"
     exit 1
 fi
 
