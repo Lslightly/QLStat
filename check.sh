caf29382441c682bfe73d4fa85891f35bc1d2fa9
lqw@128C <lqw332664203@mail.ustc.edu.cn>
2026-05-07 17:36:48 +0800

    test: add content check for malloc_time.ql in check.sh

--- check.sh
+++ check.sh
@@ -21,8 +21,17 @@ splitline
 echo "External Verify Test"
 
 go run ./cmd/batch_clone_build -noclone yaml-examples/malloc_test.yaml
+# check number of lines in pprof ext csv file is equal to count of CodeQL class instances
 if ! go run ./cmd/pprof-external-verify codeql-db/test/malloc_test/ instance_count ; then
     echo "External Verify Test for malloc_test Failed"
+    exit 1
+fi
+
+# check mallocgc time is mapped to CodeQL class instances
+if [ "$(grep -c . codeqlResult/pprof_ext/malloc_time/malloc_test.csv)" -ne 2 ]; then
+    echo "External Verify Test for mallocgc time mapping Failed"
+    echo "The content of mallocgc time csv file is:"
+    cat codeqlResult/pprof_ext/malloc_time/malloc_test.csv
     exit 1
 fi
 
